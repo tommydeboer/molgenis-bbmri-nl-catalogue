737c4cffffa4860579558078e6a935c26a57fc95
tommydeboer <tommydeboer4@gmail.com>
2017-10-05 12:30:24 +0200

    Make project compile

--- molgenis-app-bbmri-nl-catalogue/src/main/java/org/molgenis/app/controller/ContactController.java
+++ molgenis-app-bbmri-nl-catalogue/src/main/java/org/molgenis/app/controller/ContactController.java
@@ -1,11 +1,11 @@
 package org.molgenis.app.controller;
 
-import static org.molgenis.app.controller.ContactController.URI;
-
-import org.molgenis.ui.MolgenisPluginController;
 import org.molgenis.ui.controller.AbstractStaticContentController;
+import org.molgenis.web.PluginController;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import static org.molgenis.app.controller.ContactController.URI;
 
 /**
  * Controller that handles contact page requests
@@ -15,8 +15,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class ContactController extends AbstractStaticContentController
 {
 	public static final String ID = "contact";
-	public static final String URI = MolgenisPluginController.PLUGIN_URI_PREFIX + ID;
-	
+	public static final String URI = PluginController.PLUGIN_URI_PREFIX + ID;
+
 	public ContactController()
 	{
 		super(ID, URI);
