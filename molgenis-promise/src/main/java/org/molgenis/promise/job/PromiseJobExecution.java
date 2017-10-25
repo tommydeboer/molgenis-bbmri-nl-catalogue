113d4c3f3c2dbd1067c777f0fbab6e71b2b2b91a
tommydeboer <tommydeboer4@gmail.com>
2017-10-25 09:08:26 +0200

    Fix bean bootstrapping issue

--- molgenis-promise/src/main/java/org/molgenis/promise/job/PromiseJobExecution.java
+++ molgenis-promise/src/main/java/org/molgenis/promise/job/PromiseJobExecution.java
@@ -2,15 +2,29 @@ package org.molgenis.promise.job;
 
 import org.molgenis.data.Entity;
 import org.molgenis.data.jobs.model.JobExecution;
+import org.molgenis.data.meta.model.EntityType;
 import org.molgenis.promise.PromiseMapperType;
 
 import static org.molgenis.promise.job.PromiseJobExecutionMetadata.*;
 
+@SuppressWarnings("unused")
 public class PromiseJobExecution extends JobExecution
 {
 	public PromiseJobExecution(Entity entity)
 	{
 		super(entity);
+		setType(JOB_TYPE);
+	}
+
+	public PromiseJobExecution(EntityType entityType)
+	{
+		super(entityType);
+		setType(JOB_TYPE);
+	}
+
+	public PromiseJobExecution(String identifier, EntityType entityType)
+	{
+		super(identifier, entityType);
 		setType(JOB_TYPE);
 	}
 
